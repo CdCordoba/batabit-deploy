e0e53178536d446585b635f72c8981609ed21d84
CdCordoba <cd.cordoba13@gmail.com>
2022-07-23 16:44:31 -0300

    deploy

---
+++ precache-manifest.b4b455d17637701585b533560bda2ac5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "02202c414bccd772b2fb",
+    "url": "/static/css/main.a0b3246e.chunk.css"
+  },
+  {
+    "revision": "02202c414bccd772b2fb",
+    "url": "/static/js/main.02202c41.chunk.js"
+  },
+  {
+    "revision": "91d1d4c5a6814aad6413",
+    "url": "/static/js/1.91d1d4c5.chunk.js"
+  },
+  {
+    "revision": "06dcd26aaa3e75022c36",
+    "url": "/static/js/2.06dcd26a.chunk.js"
+  },
+  {
+    "revision": "c1b6e4f0035755e25821",
+    "url": "/static/js/runtime~main.c1b6e4f0.js"
+  },
+  {
+    "revision": "2bdae616efc447e9e2142b230dc3b1a7",
+    "url": "/static/media/eye.2bdae616.svg"
+  },
+  {
+    "revision": "fc4be370de19760a0dfa60c18721f25e",
+    "url": "/static/media/batata.fc4be370.svg"
+  },
+  {
+    "revision": "7f17853a6e8fb847223e069d86a2ff01",
+    "url": "/static/media/clock.7f17853a.svg"
+  },
+  {
+    "revision": "33034bc352cb54e6ad35b27788de75dd",
+    "url": "/static/media/dollar-sign.33034bc3.svg"
+  },
+  {
+    "revision": "9d9af8f263ebcd5c4a38744e44ea6e5a",
+    "url": "/static/media/check-circle.9d9af8f2.svg"
+  },
+  {
+    "revision": "5f85bc39e3dca11dde91242e6a9c587a",
+    "url": "/static/media/logo.5f85bc39.svg"
+  },
+  {
+    "revision": "94e075cf81e335c69190a1b811f5ebb6",
+    "url": "/static/media/down-arrow.94e075cf.svg"
+  },
+  {
+    "revision": "b05fc41eacb1ccd29e3b176bb1b8b9c2",
+    "url": "/static/media/trending-up.b05fc41e.svg"
+  },
+  {
+    "revision": "2e17dbd5dc5c751a8f83e0ea2445a101",
+    "url": "/static/media/trending-down.2e17dbd5.svg"
+  },
+  {
+    "revision": "86184533f411f06c5a1fc8ea4a435bd7",
+    "url": "/static/media/Bitcoin.86184533.svg"
+  },
+  {
+    "revision": "f0b3531c92edeb35715efae04c8efb5c",
+    "url": "/static/media/bitcoinbaby2x.f0b3531c.png"
+  },
+  {
+    "revision": "a649e9e3e59b6a9cd8d5a89946e5e613",
+    "url": "/static/media/orange-right-arrow.a649e9e3.svg"
+  },
+  {
+    "revision": "4e3c21d3fca6bc2dfb7b706e6fc8aa58",
+    "url": "/static/media/logo-footer.4e3c21d3.svg"
+  },
+  {
+    "revision": "9d9f1bd23b18bf7782ead17be28bc41c",
+    "url": "/index.html"
+  }
+];
